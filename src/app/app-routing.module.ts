fbbaa86f3ac3a6fd9bc2186f75b1ae58171b11e9
Abdurasul Bobonazarov <19mrasul@gmail.com>
2021-07-15 22:13:45 +0200

    This is first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FoundCreateComponent } from './layout/found-create/found-create.component';
 
-const routes: Routes = [];
+import { LayoutComponent } from './layout/layout.component';
+import { LostCreateComponent } from './layout/lost-create/lost-create.component';
+import { MainPageComponent } from './layout/main-page/main-page.component';
+import { MyProfileComponent } from './layout/my-profile/my-profile.component';
+
+
+const routes: Routes = [
+  { path: '', component: LayoutComponent, children: [
+    { path: '', component: MainPageComponent },
+    { path: 'my-profile', component: MyProfileComponent },
+    { path: 'i-found', component: FoundCreateComponent },
+    { path: 'i-lost', component: LostCreateComponent }
+  ]},
+  { path: 'en', component: LayoutComponent, children: [
+        { path: '', component: MainPageComponent },
+        { path: 'my-profile', component: MyProfileComponent },
+        { path: 'i-found', component: FoundCreateComponent },
+        { path: 'i-lost', component: LostCreateComponent }
+  ]},
+  // { path: 'administrator', component: AdministratorComponent },
+  // { path: 'log-in', component: LogInComponent }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    RouterModule.forRoot(routes,{
+      scrollPositionRestoration: 'enabled'
+    })
+  ],
+  exports: [
+    RouterModule
+  ]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImgUploaderComponent } from './layout/img-uploader/img-uploader.component';
+import { SignInComponent } from './layout/sign-in/sign-in.component';
+import { LayoutComponent } from './layout/layout.component';
+import { MainPageComponent } from './layout/main-page/main-page.component';
+import { MyProfileComponent } from './layout/my-profile/my-profile.component';
+import { FoundCreateComponent } from './layout/found-create/found-create.component';
+import { LostCreateComponent } from './layout/lost-create/lost-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImgUploaderComponent,
+    SignInComponent,
+    LayoutComponent,
+    MainPageComponent,
+    MyProfileComponent,
+    FoundCreateComponent,
+    LostCreateComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MDBBootstrapModule.forRoot(),
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
